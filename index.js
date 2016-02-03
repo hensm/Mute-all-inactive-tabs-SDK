6d3bc45623d646e924776186ebbbbfe975b11508
Matt F <marcii@cssu.in>
2016-02-03 08:27:14 +0000

    Cleanup

--- index.js
+++ index.js
@@ -2,26 +2,32 @@ const { viewFor } = require("sdk/view/core");
 
 const tabs = require("sdk/tabs");
 const tabs_utils = require("sdk/tabs/utils");
-const window_utils = require("sdk/window/utils");
 const simple_prefs = require("sdk/simple-prefs");
-
-function tabFilter(tab) {
-	return (simple_prefs.prefs["affectAllWindows"]
-			? (tab !== viewFor(tabs.activeTab))
-			: (!tab.selected)) && (tab.getAttribute("soundplaying") === "true");
-}
 
 
 function setMuted(tab, muted) {
-	if ((muted && tab.getAttribute("muted") !== "true")
-			|| (!muted && tab.getAttribute("muted") === "true")) {
-		tab.toggleMuteAudio();
+	let browser = tabs_utils.getBrowserForTab(tab);
+	if (muted) {
+		browser.mute();
+		tab.setAttribute("muted", "true");
+	} else {
+		browser.unmute();
+		tab.removeAttribute("muted");
 	}
 }
 
 function updateInactive() {
 	let tabs_arr = tabs_utils.getTabs();
-	tabs_arr.filter(tabFilter).forEach(tab => setMuted(tab, true));
+	tabs_arr.filter(tab => {
+		let selected = tab.getAttribute("selected");
+		let audible = tab.getAttribute("soundplaying") === "true";
+
+		if (simple_prefs.prefs["affectAllWindows"]) {
+			return (tab !== viewFor(tabs.activeTab)) && audible;
+		} else {
+			return !selected && audible;
+		}
+	}).forEach(tab => setMuted(tab, true));
 }
 
 tabs.on("activate", tab => {
